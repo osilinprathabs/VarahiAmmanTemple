88f72c6271e94628513195a092f1dfcb085d7152
Prabha Karan <prabhakaran@Prabhas-MacBook-Pro.local>
2025-02-20 00:24:40 +0530

    Initial commit for Varahi Amman Temple project

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,  // Specify the port you want to use
+  },
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg', '**/*.JPG']
 });
